a4d34b994437418f309bb5ebac78a48bfb1f8af4
Clinton Huling <chuling0@hotmail.com>
2014-04-10 13:23:35 -0600

    Email Setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,19 @@ Depot::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "test.net",
+    :authentication => 'plain',
+    :user_name => 'gwclint',
+    :password => 'Screwdat32',
+    :enable_starttls_auto => true
+  }
+
+  
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
